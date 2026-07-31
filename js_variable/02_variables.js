b639fb1b5eed994ccf7b54fc5fdc00c2db79de71
Aditya Negi <01adityanegi@gmail.com>
2026-07-31 15:50:07 +0000

    create 02_variables

---
+++ js_variable/02_variables.js
@@ -0,0 +1,23 @@
+// Here i create a variable and store the college details . 
+/*Store your college name.
+Store your branch.
+Store your roll number.
+Store your semester.
+Store your college email.
+Store your CGPA.
+Store your attendance percentage.
+Store whether your fees are paid (true/false).
+Store your class section.
+Print all the college details.*/
+
+const collegeName = "ABD Institute of Technology" //This value is constent this is not changable 
+const branch = "CSE"
+const rollNo = 2500001
+const semester = 6
+const collegeEmail = "aa@abd.com"
+const cgpa = 8.9
+const attendancePercentage = 77
+const feesPaid = true
+const classSection = "A"
+
+console.table([collegeName,branch,rollNo,semester,collegeEmail,cgpa,attendancePercentage,feesPaid,classSection])
