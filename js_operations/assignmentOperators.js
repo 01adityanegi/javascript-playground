f1e77a08e5f470e8a7be7f7a23a3935a8c06568d
Aditya Negi <01adityanegi@gmail.com>
2026-08-02 16:34:19 +0000

    Assignment Operators

---
+++ js_operations/assignmentOperators.js
@@ -0,0 +1,38 @@
+//Assignment Operators (=, +=, -=, *=, /=, %=) 
+// These operators are used to assign values to variables and perform arithmetic operations in a concise manner.
+
+// Increase a bank balance by ₹5,000 using +=.
+
+let bankBalance = 10000
+bankBalance += 5000
+console.log("Your bank Balance is : ", bankBalance)
+
+// Decrease a wallet balance after shopping using -=.
+let walletBalance = 5000
+walletBalance -= 4599
+console.log("You Wallet Balance after Shopping is : ", walletBalance);
+
+// Double a player's score using *=.
+
+
+// Divide a company's profit equally among departments using /=.
+
+
+// Find the remainder using %=.
+
+
+// Increase your age by 1 using +=.
+
+
+// Decrease the stock quantity by 10.
+
+
+// Triple your monthly savings.
+
+
+// Halve your mobile data balance.
+
+
+// Update your game coins after spending some.
+
+
