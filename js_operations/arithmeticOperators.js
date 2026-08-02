deb6cf1e8127c81a2da538c7fc11b7bc14ad3844
Aditya Negi <01adityanegi@gmail.com>
2026-08-02 15:52:34 +0000

    ArithmeticOperators

---
+++ js_operations/arithmeticOperators.js
@@ -0,0 +1,33 @@
+// Arithmetic Operators (+,- , * , / , % ,** ) : whose perfome the operations between two operatins. 
+
+// Store the price of two books and calculate the total price.
+let theHistoryBook = 299
+let theScienceBook = 499
+totalPriceOfBooks = theHistoryBook+theScienceBook //here the addition operations perform . 
+console.log("The Total price of books is : ", totalPriceOfBooks)
+
+// Divide ₹500 equally among 4 friends.
+
+
+// Find the remainder when 125 is divided by 12.
+
+
+// Calculate the square of a number using **.
+
+
+// Calculate the cube of a number.
+
+
+// Convert 2 hours into minutes using multiplication.
+
+
+// Convert 5 days into hours.
+
+
+// Find the total seconds in 3 hours.
+
+
+// Calculate the total marks of 5 subjects.
+
+
+// Find the average marks of 5 subjects.
