00b03e0dad064084b4d766ef03eefb4e03ad09d7
Aditya Negi <01adityanegi@gmail.com>
2026-08-02 16:41:03 +0000

    Assignment operators question solve

--- js_operations/assignmentOperators.js
+++ js_operations/assignmentOperators.js
@@ -13,13 +13,19 @@ walletBalance -= 4599
 console.log("You Wallet Balance after Shopping is : ", walletBalance);
 
 // Double a player's score using *=.
-
+let PlayerScore = 100
+PlayerScore *= 2
+console.log("Player's New score is : ", PlayerScore)
 
 // Divide a company's profit equally among departments using /=.
-
+let companyProfit = 100000
+companyProfit /= 5
+console.log("The compay's profit equal divide in 5 deparments is : " , companyProfit)
 
 // Find the remainder using %=.
-
+let findRemainder = 30
+findRemainder %= 4
+console.log("The remainder is : ", findRemainder)
 
 // Increase your age by 1 using +=.
 
