27b832fe671f95204fab9627c934c38c064fd5ba
Aditya Negi <01adityanegi@gmail.com>
2026-08-02 16:11:19 +0000

    Arithmetic Operators question solve

--- js_operations/arithmeticOperators.js
+++ js_operations/arithmeticOperators.js
@@ -9,26 +9,35 @@ console.log("The Total price of books is : ", totalPriceOfBooks)
 // Divide ₹500 equally among 4 friends.
 let totalAmount = 500
 let friends = 4 
-console.log("The 500 divide in 4 friend is : " ,totalAmount/friends, "per friend. ")
+console.log("The 500 divide in 4 friend is : " ,totalAmount/friends, "per friend. ") //here the divide operator perform . 
 
 // Find the remainder when 125 is divided by 12.
-
+let value = 125
+let dividedValue = 12 
+console.log("The remainder is : ",value%dividedValue)
 
 // Calculate the square of a number using **.
-
+let squareNumber = 5
+console.log("The square of number is : ",squareNumber**2)
 
 // Calculate the cube of a number.
-
+let cubeOfNumber = 5
+console.log("The Cube of number is : ",cubeOfNumber**3)
 
 // Convert 2 hours into minutes using multiplication.
-
+let hours = 2
+let minutes = 60
+console.log("The 2 hours into minutes is : ",hours*minutes)
 
 // Convert 5 days into hours.
-
+let days = 5
+let oneDayhour = 24
+console.log("The 5 days into hours is : ", days * oneDayhour)
 
 // Find the total seconds in 3 hours.
-
-
+let hours2 = 3
+let seconds = 3600 // 1 hours = 3600 sec
+console.log("The 3 hours into secounds is : ", hours2 * seconds)
 // Calculate the total marks of 5 subjects.
 
 
