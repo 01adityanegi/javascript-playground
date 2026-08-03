bec715eff191f4b6bb0c8b982b5f8f89c392dcd0
Aditya Negi <01adityanegi@gmail.com>
2026-08-03 07:10:07 +0000

    Increment & Decrement

---
+++ js_operations/incrementDecrementOp.js
@@ -0,0 +1,36 @@
+// Increment & Decrement Operators (++, --)
+
+// Increase the page number using ++.
+let pageNo = 5
+pageNo++
+console.log(pageNo)
+
+// Decrease a countdown timer using --.
+let countdownTimer = 3
+countdownTimer-- //here value is 3 its look like 3-- 
+countdownTimer-- //here value is 2 
+countdownTimer-- //here value is 1 
+console.log(countdownTimer) //then here value is 0 
+
+// Print the value before and after count++.
+
+
+// Print the value before and after ++count.
+
+
+// Print the value before and after count--.
+
+
+// Print the value before and after --count.
+
+
+// Increase the number 10 times manually using ++.
+
+
+// Decrease a variable until it reaches 0.
+
+
+// Simulate visitor count increasing.
+
+
+// Simulate available seats decreasing.
