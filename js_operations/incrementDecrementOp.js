4c37db9fe0478dab9d8b8f98407e2f8ba4eee8ce
Aditya Negi <01adityanegi@gmail.com>
2026-08-03 07:34:19 +0000

    Increment Decrement

--- js_operations/incrementDecrementOp.js
+++ js_operations/incrementDecrementOp.js
@@ -13,16 +13,28 @@ countdownTimer-- //here value is 1
 console.log(countdownTimer) //then here value is 0 
 
 // Print the value before and after count++.
-
+let counterValue = 10
+console.log("Before Cout++ :" , counterValue);
+counterValue++
+console.log("After Cout++ :" , counterValue);
 
 // Print the value before and after ++count.
-
+let counterValue2 = 15
+console.log("Before ++Cout :" , counterValue2);
+++counterValue2 // here is value is 16 
+console.log("After ++Cout :" , counterValue2);
 
 // Print the value before and after count--.
-
+let counterSubValue = 20
+console.log("Before Cout-- :" , counterSubValue);
+counterSubValue--
+console.log("After Cout-- :" , counterSubValue);
 
 // Print the value before and after --count.
-
+let countersubValue = 5
+console.log("Before  -- Cout :" , countersubValue);
+--countersubValue
+console.log("After -- Cout :" , countersubValue);
 
 // Increase the number 10 times manually using ++.
 
