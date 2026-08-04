e3634d2b71e22425e49977142606753a873f7bdd
Aditya Negi <01adityanegi@gmail.com>
2026-08-04 05:33:23 +0000

    String

---
+++ jsString/01_string.js
@@ -0,0 +1,33 @@
+// Create a string variable named name and store your name in it. Print it.
+let Name = "Aadi" //this is sting . 
+console.log(Name) 
+
+// Create a string "JavaScript" and print its length.
+let langName = "javaScript"
+console.log(langName.length) //this is length method whose count the string length. 
+
+// Print the first character of "Programming".
+let lang = "Programming"
+console.log(lang[0]) //this is index value its start with 0 .
+
+// Print the last character of "Programming".
+console.log(lang[10])
+
+// Convert "javascript" into uppercase.
+console.log(langName.toUpperCase()) //toUppercase convert string into Upper case :- JAVASCRIPT
+
+// Convert "JAVASCRIPT" into lowercase.
+console.log(langName.toLowerCase()) //toLowerCase convert string into lower case :- javascript
+
+// Remove extra spaces from " Hello World ".
+let Word  = "  Hello world  "
+console.log(Word)
+console.log(Word.trim()) //trim method remove the white space form begining and end of the sting . 
+
+// Check whether "JavaScript" contains "Script".
+
+
+// Check whether "JavaScript" starts with "Java".
+
+
+// Check whet her "JavaScript" ends with "Script".
