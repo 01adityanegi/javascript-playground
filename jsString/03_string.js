da53ac547856133e942c0debe070d07074e7d74d
Aditya Negi <01adityanegi@gmail.com>
2026-08-04 06:48:35 +0000

    string

---
+++ jsString/03_string.js
@@ -0,0 +1,24 @@
+// Repeat the string "Hello " 5 times.
+let word = "\tHello"
+console.log(word.repeat(5))
+// Convert "7" into "007" using padStart().
+let value = "7"
+console.log(value.padStart("3","0"))
+// Convert "7" into "700" using padEnd().
+console.log(value.padEnd(3,0))
+// Check whether "OpenAI" contains "AI".
+let aiCompany = "OpneAI"
+console.log(aiCompany.includes("AI"))
+// Extract "Developer" from "Frontend Developer".
+let developer = "Frontend Developer"
+console.log(developer.slice(8,))
+// Replace "blue" with "red" in "My car is blue.".
+let sentence = "My car is blue"
+console.log(sentence.replace("blue" , "red"))
+// Split "I love JavaScript" into an array of words.
+
+// Count the number of characters in "OpenAI GPT".
+
+// Print the second character of "Computer".
+
+// Print the second-last character of "Computer" using .at().
