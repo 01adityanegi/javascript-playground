539db5bb7210b46a76a47fb2e86324f3938f6eca
Aditya Negi <01adityanegi@gmail.com>
2026-08-04 05:53:26 +0000

    stirng

---
+++ jsString/02_string.js
@@ -0,0 +1,40 @@
+// Find the first index of "a" in "banana".
+let fruitName = "banana"
+console.log(fruitName.indexOf("a"))
+
+
+// Find the last index of "a" in "banana".
+console.log(fruitName.lastIndexOf("a"))
+
+
+// Extract "Script" from "JavaScript".
+let langName = "JavaScript"
+console.log(langName.slice(4, ))
+
+// Extract "Java" from "JavaScript".
+console.log(langName.slice( 0,4))
+
+// Replace "Java" with "Python" in "I love Java".
+let myLanguage = "I love Java"
+console.log(myLanguage.replace("Java", "Python"))
+
+
+// Replace every "cat" with "dog" in "cat cat cat".
+
+
+
+// Split "apple,banana,mango" into an array.
+
+
+
+// Join "Hello" and " World" into one string.
+
+
+
+// Print the character at index 5 in "JavaScript".
+
+
+
+// Print the last character of "JavaScript" using .at().
+
+
