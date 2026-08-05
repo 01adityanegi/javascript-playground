e21182bfb03bfefffecd685a441ebce4f5137ef7
Aditya Negi <01adityanegi@gmail.com>
2026-08-05 16:31:12 +0000

    Number Methods in js

---
+++ js_number/01_number.js
@@ -0,0 +1,30 @@
+//Here are the Number methods . 
+// Convert the number 255 into a string.
+const numValue = new Number(255)
+console.log(numValue.toString())
+console.log(typeof numValue) //here is the type of numValue is object . because we use the Number constructor. 
+ 
+// Convert 255 into binary.
+console.log(numValue.toString(2))
+// Convert 255 into octal.
+console.log(numValue.toString(8))
+// Convert 255 into hexadecimal.
+console.log(numValue.toString(16))
+
+// Display 12.34567 with exactly 2 decimal places.
+const numDecimal = new Number (12.34567)
+console.log(numDecimal.toFixed(2))
+
+// Display 8.1 with exactly 4 decimal places.
+
+
+// Round 99.9999 to 3 decimal places.
+
+
+// Convert 12345 into exponential notation.
+
+
+// Convert 0.000456 into exponential notation with 2 decimal places.
+
+
+// Format 123.456789 to 4 significant digits.
