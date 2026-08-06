647d1ac25f08027f5a4b9b7b1c00c911b8b396af
Aditya Negi <01adityanegi@gmail.com>
2026-08-06 15:16:10 +0000

    JS number method

--- js_number/02_number.js
+++ js_number/02_number.js
@@ -19,15 +19,22 @@ const valueNumSecound = new Number (123456789)
 console.log(valueNumSecound.toLocaleString("en-IN"))
 
 // Format 2500000.75 as US currency.
-
+const usCurrency = new Number(2500000.75)
+console.log(usCurrency.toLocaleString())
 
 // Format 2500000.75 as Indian Rupees.
-
+const indCurrency = new Number(2500000.75)
+console.log(indCurrency.toLocaleString("en-IN"))
 
 // Format 0.75 as a percentage.
-
+const percentageCon = new Number (0.75)
+console.log(`${percentageCon * 100 }%`)
 
 // Display 12345.6789 with a maximum of 2 decimal places.
-
+const decimalValue = 12345.6789
+console.log(decimalValue.toFixed(2))
 
 // Convert 1234.56789 into a string, round it to 2 decimal places, and display it with commas.
+const valueConvert = new Number(1234.56789)
+console.log(`${valueConvert.toString()} , ${valueConvert.toFixed(2)} , ${valueConvert.toLocaleString("en-IN")}`)
+
