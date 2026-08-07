97016831b12b358881980483e1b833bddf16d9fa
Aditya Negi <01adityanegi@gmail.com>
2026-08-07 07:18:56 +0000

    Numebr methods in Java Script

--- js_number/02_number.js
+++ js_number/02_number.js
@@ -31,10 +31,34 @@ const percentageCon = new Number (0.75)
 console.log(`${percentageCon * 100 }%`)
 
 // Display 12345.6789 with a maximum of 2 decimal places.
-const decimalValue = 12345.6789
+const decimalValue = new Number(12345.6789)
 console.log(decimalValue.toFixed(2))
 
 // Convert 1234.56789 into a string, round it to 2 decimal places, and display it with commas.
 const valueConvert = new Number(1234.56789)
 console.log(`${valueConvert.toString()} , ${valueConvert.toFixed(2)} , ${valueConvert.toLocaleString("en-IN")}`)
+
+// Convert a decimal number into binary, octal, and hexadecimal.
+const decimalNum = new Number (14.56)
+console.log(decimalNum.toString(2))
+console.log(decimalNum.toString(8))
+console.log(decimalNum.toString(16))
+
+
+// Display a bill amount in both Indian and US currency formats.
+const billAmount = new Number(150000)
+console.log(`In us currency :$ ${billAmount.toLocaleString()}`)
+console.log(`In Indian Currency :${billAmount.toLocaleString("en-IN")}₹`)
+
+// Create a Number object and print its string, primitive, fixed, and exponential values.
+const valueThree = new Number (25)
+console.log(valueThree.toString())
+console.log(valueThree.valueOf())
+console.log(valueThree.toFixed(1))
+console.log(valueThree.toExponential(1))
+
+// Compare the outputs of toFixed(), toPrecision(), and toExponential() using the same number.
+
+
+// Create a program that demonstrates all major JavaScript Number methods on a single number.
 
