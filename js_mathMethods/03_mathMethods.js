d459abd6a6ab88ec2727effc09850640ecde5083
Aditya Negi <01adityanegi@gmail.com>
2026-08-07 15:40:59 +0000

    Math Methods

---
+++ js_mathMethods/03_mathMethods.js
@@ -0,0 +1,43 @@
+// Generate a random OTP of 4 digits.
+const minValue = 1111
+const maxValue = 9999
+const otplogic = (maxValue - minValue +1) + minValue
+console.log(`Your OTP IS : ${Math.floor(Math.random()* otplogic)}` )
+
+// Generate a random OTP of 6 digits.
+const minValueInSixDigits = 111111
+const maxValueInSixDigits = 999999
+const sixDigtsOtp = (maxValueInSixDigits - minValueInSixDigits +1) + minValueInSixDigits
+console.log(`Your OTP IS : ${Math.floor(Math.random()* sixDigtsOtp)}` )
+
+// Simulate rolling a dice.
+const minValueForRolling = 1
+const maxValueForRolling = 6
+const diceRolling = (maxValueForRolling - minValueForRolling +1) + minValueForRolling
+console.log(`Your number is : ${Math.floor(Math.random()* diceRolling)}`)
+
+// Simulate flipping a coin.
+
+
+// Generate a random RGB color.
+
+
+// Generate a random HEX color.
+
+
+// Find the largest value in an array.
+
+
+// Find the smallest value in an array.
+
+
+// Calculate the area of a circle.
+
+
+// Calculate the circumference of a circle.
+
+
+// Round a number to 2 decimal places.
+
+
+// Check whether a number is a perfect square.
