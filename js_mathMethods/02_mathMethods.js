b206207387ed642090d1d142aad655b93a5921ff
Aditya Negi <01adityanegi@gmail.com>
2026-08-07 14:50:19 +0000

    Math Methods

---
+++ js_mathMethods/02_mathMethods.js
@@ -0,0 +1,37 @@
+// Generate a random integer between 1 and 10.
+const minValue = 1
+const maxValue = 10
+console.log(Math.floor(Math.random()*(maxValue - minValue +1)+ minValue))
+
+// Generate a random integer between 1 and 100.
+const secondMaxvalue = 100
+console.log(Math.floor(Math.random()*(secondMaxvalue - minValue +1)+ minValue))
+
+// Find Math.pow(2, 10).
+console.log(Math.pow(2,10))
+
+// Find Math.sqrt(625).
+console.log(Math.sqrt(625))
+
+// Find Math.cbrt(343).
+console.log(Math.cbrt(343))
+
+// Find Math.log10(1000).
+console.log(Math.log10(1000))
+
+// Find Math.log2(64).
+
+
+// Find Math.exp(2).
+
+
+// Find Math.sin(Math.PI / 2).
+
+
+// Find Math.cos(0).
+
+
+// Find Math.tan(Math.PI / 4).
+
+
+// Find the hypotenuse of a 6-8 triangle.
