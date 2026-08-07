10641817abe05c782cccc1daa6ce1afacbbea2bf
Aditya Negi <01adityanegi@gmail.com>
2026-08-07 08:00:34 +0000

    Math Methods

---
+++ js_mathMethods/01_mathMethods.js
@@ -0,0 +1,46 @@
+//Here we learn math methods in java script . 
+
+// Print the value of Math.PI.
+console.log(Math.PI)
+
+// Print the value of Math.E.
+console.log(Math.E)
+
+// Find the absolute value of -25.
+let valueInNumber = new Number(-25)
+console.log(Math.abs(valueInNumber))
+// Find the square root of 144.
+let value = new Number(144)
+console.log(Math.sqrt(value))
+
+// Find the cube root of 125.
+let CubeRootValue = new Number(125)
+console.log(Math.cbrt(CubeRootValue))
+
+// Find 5 raised to the power of 4.
+let baseValue = new Number(5)
+let raisedaPowerValue = new Number(4)
+console.log(Math.pow(baseValue, raisedaPowerValue))
+
+// Find the maximum of 10, 45, 23, 89, 12.
+
+
+// Find the minimum of 10, 45, 23, 89, 12.
+
+
+// Round 7.6 to the nearest integer.
+
+
+// Round down 8.9.
+
+
+// Round up 8.1.
+
+
+// Remove the decimal part from 15.99.
+
+
+// Find the sign of -15.
+
+
+// Generate a random decimal number.
