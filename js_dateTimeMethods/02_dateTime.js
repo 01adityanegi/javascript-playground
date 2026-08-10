ab9746435823c6062e4e2d267a0990f6929a9ccc
Aditya Negi <01adityanegi@gmail.com>
2026-08-10 16:23:54 +0000

    Date And Time Methods

--- js_dateTimeMethods/02_dateTime.js
+++ js_dateTimeMethods/02_dateTime.js
@@ -27,3 +27,41 @@ console.log(minutes.getMinutes())
 let seconds  = new Date()
 seconds.setSeconds(45)
 console.log(seconds.getSeconds())
+
+//Create a date for:
+// 1 January 2025
+// Then change it to:
+// 15 August 2030
+
+let fullYear = new Date(2025, 0, 1);
+console.log(fullYear.toLocaleString("en-IN", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    }))
+fullYear.setFullYear(2030, 7, 15);
+console.log(
+    fullYear.toLocaleString("en-IN", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    })
+);
+
+// Create a date and print the result using:
+// toString()
+// toDateString()
+// toTimeString()
+
+let dateResult = new Date()
+console.log(dateResult.toString())
+console.log(dateResult.toDateString())
+console.log(dateResult.toTimeString())
+
+// Create a date and convert it into an ISO string using toISOString().
+let isoDate = new Date()
+console.log(isoDate.toISOString())
+
+// Display today's date using toLocaleDateString("en-IN").
+let isoLocalDate = new Date()
+console.log(isoLocalDate.toLocaleDateString("en-IN"))
