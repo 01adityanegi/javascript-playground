49b31581d566b00fa9edd171008a2cb484b903cd
Aditya Negi <01adityanegi@gmail.com>
2026-08-10 14:57:44 +0000

    Date and Time Methods

---
+++ js_dateTimeMethods/02_dateTime.js
@@ -0,0 +1,9 @@
+// Create a date and change its year to 2030 using setFullYear().
+let year = new Date();
+year.setFullYear(2030);
+console.log(year.getFullYear());
+// Create a date and change its month to December using setMonth().
+// Create a date and change its day to 25 using setDate().
+// Create a date and change its hour to 10.
+// Create a date and change its minutes to 30.
+// Create a date and change its seconds to 45.
