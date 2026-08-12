d433e905e20cf6a62b7e9d19294128c646ce3c92
Aditya Negi <01adityanegi@gmail.com>
2026-08-12 14:26:30 +0000

    Array In js

---
+++ js_array./02_array.js
@@ -0,0 +1,30 @@
+// Create an array of numbers and use includes() to check whether 50 exists.
+let numArray = [34, 64, 100, 70, 50 , 24]
+console.log(numArray.includes(50))
+
+// Find the position of a particular element using indexOf().
+console.log(numArray.indexOf(64))//if the vale exist its give index no . 
+console.log(numArray.indexOf(44))//if the vale not exist its give -1. 
+
+// Reverse an array using reverse().
+console.log(numArray.reverse())
+
+// Sort an array of numbers in ascending order.
+console.log(numArray.sort((a, b)=> a-b))
+
+// Sort an array of numbers in descending order.
+console.log(numArray.sort((a, b)=> b-a))
+
+// Join all elements of an array into one string using join().
+
+
+// Create a copy of an array using slice().
+
+
+// Remove 2 elements from the middle using splice().
+
+
+// Add 2 new elements in the middle using splice().
+
+
+// Replace an element using splice().
