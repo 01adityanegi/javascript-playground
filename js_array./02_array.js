b61a0fa6b41b0d761ba564642b1ab436478bcdcc
Aditya Negi <01adityanegi@gmail.com>
2026-08-12 14:48:53 +0000

    Array

--- js_array./02_array.js
+++ js_array./02_array.js
@@ -22,13 +22,24 @@ console.log(numArraytwo.join()) //change array into string .
 // Create a copy of an array using slice().
 let numArrayThree = numArray.slice()
 // Remove 2 elements from the middle using splice().
-console.log(numArrayThree.splice(2,3))
+console.log(numArrayThree.splice(2,2))
 
 // Add 2 new elements in the middle using splice().
 let newArray = [4, 64, 3 ,35, 61 , 55 , 99]
 newArray.splice(3, 0, 100, 200);
 console.log(newArray);
 
+// add the new element using splice 
+// 3              → start at index 3
+// 0              → delete 0 elements
+// 100, 200       → add these 2 elements
+
 // Replace an element using splice().
 newArray.splice(5,1,500)
 console.log(newArray);
+
+// in splice replace . 
+// newArray.splice(5,1,500)
+//  index → position of the element you want to replace like 5
+// 1 → delete one element 
+// newValue → value you want to put there example 500 
