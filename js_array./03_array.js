1a4c32c87a52ea51482976dbe2be716e188511f7
Aditya Negi <01adityanegi@gmail.com>
2026-08-14 21:11:43 +0530

    Implement number filtering and counting in array
    Added functionality to count and print numbers greater than 50 and smaller than 20.

--- js_array./03_array.js
+++ js_array./03_array.js
@@ -52,12 +52,36 @@ for (let i = 0 ; i< evenOdd.length; i++){
 console.log(odd)
 
 // Print only numbers greater than 50.
+let numArray = [45, 64, 34 ,31,56,89, 90 ,  23 ,399 , 589, 2 , 5783, 5, 455 ]
+let countTwo = 0 
+for (let i = 0; i<numArray.length; i++){
+    if(numArray[i]>50){
+        console.log(numArray[i])
+        countTwo++
+    }
+}
+console.log(`Total numbers greater than 50 => ${countTwo}`);
 
 
 // Print only numbers smaller than 20.
 
-
-// Count how many numbers are greater than 50.
+let smallerNum = [3, 5, 64, 43 , 78 , 99 , 7 , 12 , 15 ]
+let countThree = 0 
+for (let i = 0 ; i<smallerNum.length; i++){
+    if(smallerNum[i]<20){
+        console.log(smallerNum[i])
+        countThree++
+    }
+}
+console.log(`Totel number smaller than 20 => ${countThree}`)
 
 
 // Create a new array containing the square of every number.
+let squareArray = [2, 3, 4, 5, 6, 7];
+let squares = [];
+
+for (let i = 0; i < squareArray.length; i++) {
+    squares.push(squareArray[i] ** 2);
+}
+
+console.log(squares);
