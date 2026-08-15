0f293b46010ea8adce04c06338564816f006063f
Aditya Negi <01adityanegi@gmail.com>
2026-08-15 16:28:05 +0000

    ObjectBasics

---
+++ js_object/01_objectBacis.js
@@ -0,0 +1,39 @@
+// Create an object student with name, age, course, and college. Print the student's name.
+let student = {
+    Name : "Sujal",
+    age : 20,
+    course : "BCOM",
+    college : "BE-Serious"
+}
+console.log(`The Student Name is :  ${student["Name"]}`)
+
+// Create a car object with brand, model, and year. Print all its properties.
+let Car = {
+    brand : "Rolls-Royce",
+    model : "Series II (2025–Present)",
+    year : 2026
+}
+console.log(`The details of Car is :\nBrand : ${Car["brand"]} \nModel : ${Car["model"]} \nand year : ${Car["year"]}`)
+
+// Access an object's property using both dot notation and bracket notation.
+
+
+// Change the value of an existing property in an object.
+
+
+// Add a new property to an existing object.
+
+
+// Delete a property from an object.
+
+
+// Check whether a particular property exists in an object.
+
+
+// Create a product object and calculate its total price using price and quantity.
+
+
+// Create an object containing marks of 5 subjects and calculate the total marks.
+
+
+// Find the average marks from an object.
