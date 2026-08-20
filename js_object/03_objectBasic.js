6426a348f1089f53fa678ec0d3a2c8c305bab4a2
Aditya Negi <01adityanegi@gmail.com>
2026-08-20 15:12:55 +0000

    Object

--- js_object/03_objectBasic.js
+++ js_object/03_objectBasic.js
@@ -18,7 +18,8 @@ student.address.state = "Uttarakhand"
 console.log(`State name change into : ${student.address.state}`)
 
 // Add a pincode property inside the nested address object.
-
+student.address.pincode = 263623;
+console.log(student.address.pincode);
 console.log(Object.assign(student.address , { pincode: 263623 }))
 
 // Delete the city property from a nested address object.
@@ -27,6 +28,22 @@ delete student.address.city
 console.log(Object.entries(student))
 
 // Create a nested company → employee → name object and print the employee's name.
-
+let companyData ={
+    company :{
+        employee :{
+            name : "Rahul"
+    }
+    }
+}
+console.log(companyData.company.employee.name)
 
 // Create a user → contact → phone nested object and change the phone number.
+let userData = {
+    user : {
+        contact : {
+            phoneNo : 9876543210
+        }
+    }
+}
+userData.user.contact.phoneNo = 9987654321
+console.log(userData.user.contact.phoneNo)
