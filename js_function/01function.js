cccbfc67364bc9188cb243378119776d056612a9
Aditya Negi <01adityanegi@gmail.com>
2026-08-21 15:19:20 +0000

    Function

--- js_function/01function.js
+++ js_function/01function.js
@@ -5,6 +5,7 @@ function greeting(){
 greeting()
 
 // Create a function that takes a name as a parameter and prints the name.
+
 function greaterUser(name){           // (name) This is parameter .
     console.log(`Hii ${name}`)
 }
@@ -13,9 +14,17 @@ greaterUser("Gangadhar") //the is argument
 
 // Create a function that takes two numbers and returns their sum.
 
+function sumOfNum(num1,num2){
+        return num1+num2
+}
+console.log(`The sum of two numbers is : ${sumOfNum(5,15)}`)
 
 // Create a function that takes two numbers and returns their difference.
 
+function differenceOfNum(num3,num4){
+        return num3-num4
+}
+console.log(`The difference of two numbers is : ${differenceOfNum(55,15)}`)
 
 // Create a function that takes two numbers and returns their multiplication.
 
