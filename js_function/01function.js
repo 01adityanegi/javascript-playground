e3f0bbaf8b3065f6b111a7b66d1d6bfbe548c0fc
Aditya Negi <01adityanegi@gmail.com>
2026-08-21 15:24:51 +0000

    Function

--- js_function/01function.js
+++ js_function/01function.js
@@ -14,22 +14,32 @@ greaterUser("Gangadhar") //the is argument
 
 // Create a function that takes two numbers and returns their sum.
 
-function sumOfNum(num1,num2){
-        return num1+num2
+function sumOfNum(num,numtwo){
+        return num+numtwo               //the return is return the value in sumOfNum
 }
 console.log(`The sum of two numbers is : ${sumOfNum(5,15)}`)
 
 // Create a function that takes two numbers and returns their difference.
 
-function differenceOfNum(num3,num4){
-        return num3-num4
+function differenceOfNum(num,numtwo){
+        return num-numtwo
 }
 console.log(`The difference of two numbers is : ${differenceOfNum(55,15)}`)
 
 // Create a function that takes two numbers and returns their multiplication.
 
+function multiplicationOfNum(num,numtwo){
+        return num*numtwo
+}
+console.log(`The Multiplication of two numbers is : ${multiplicationOfNum(7,2)}`)
 
 // Create a function that takes two numbers and returns their division.
+
+function multiplicationOfNum(num,numtwo){
+        let division = num/numtwo
+        return division
+}
+console.log(`The Divison of two numbers is : ${multiplicationOfNum(98,2)}`)
 
 
 // Create a function that takes a number and returns its square.
