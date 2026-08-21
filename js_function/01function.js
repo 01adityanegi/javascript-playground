04541d531d753ed5e48603d740f577b27e41c026
Aditya Negi <01adityanegi@gmail.com>
2026-08-21 15:14:10 +0000

    Functions

---
+++ js_function/01function.js
@@ -0,0 +1,35 @@
+//Create a function that prints "Hello World".
+function greeting(){
+    console.log("Hello World")
+}
+greeting()
+
+// Create a function that takes a name as a parameter and prints the name.
+function greaterUser(name){           // (name) This is parameter .
+    console.log(`Hii ${name}`)
+}
+greaterUser("Himanshu") 
+greaterUser("Gangadhar") //the is argument
+
+// Create a function that takes two numbers and returns their sum.
+
+
+// Create a function that takes two numbers and returns their difference.
+
+
+// Create a function that takes two numbers and returns their multiplication.
+
+
+// Create a function that takes two numbers and returns their division.
+
+
+// Create a function that takes a number and returns its square.
+
+
+// Create a function that takes a number and returns its cube.
+
+
+// Create a function that takes three numbers and returns their average.
+
+
+// Create a function that takes two numbers and returns the greater number.
