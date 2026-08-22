9f6bafa9ee7c50ea782445e04adfab07ee91261c
Aditya Negi <01adityanegi@gmail.com>
2026-08-22 15:57:47 +0000

    02 Functions questions solve

---
+++ js_function/02function.js
@@ -0,0 +1,44 @@
+// Create a function that takes an array and prints the array.
+
+function valueArray(){
+    arr = [34,53,4,6,2,74]
+    return arr
+}
+console.log(valueArray())
+
+// Create a function that takes an array and returns its first element.
+function arrtwo(){
+    arr = [5,1,90,84,23]
+    return arr[0]
+}
+console.log(`The first element of array ${arrtwo()}`)
+
+// Create a function that takes an array and returns its last element.
+
+
+
+// Create a function that takes an array and returns the number of elements in it.
+
+
+
+// Create a function that takes an array of numbers and returns the sum of the numbers.
+
+
+
+// Create a function that takes an array of numbers and returns the largest number.
+
+
+
+// Create a function that takes an array of numbers and returns the smallest number.
+
+
+
+// Create a function that takes an array and a number, then checks whether that number exists in the array.
+
+
+
+// Create a function that takes an array and returns a new array containing only numbers greater than 50.
+
+
+
+// Create a function that takes an array and returns a new array containing only even numbers.
