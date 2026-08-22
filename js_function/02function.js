43c0aa1dd28fefa9d552c775054634fb4cfdf682
Aditya Negi <01adityanegi@gmail.com>
2026-08-22 16:19:54 +0000

    Function

--- js_function/02function.js
+++ js_function/02function.js
@@ -1,7 +1,7 @@
 // Create a function that takes an array and prints the array.
 
 function valueArray(){
-    arr = [34,53,4,6,2,74]
+    let arr = [34,53,4,6,2,74]
     return arr
 }
 console.log(valueArray())
@@ -9,7 +9,7 @@ console.log(valueArray())
 // Create a function that takes an array and returns its first element.
 
 function arry(){
-    arr = [5,1,90,84,23]
+   let arr = [5,1,90,84,23]
     return arr[0]
 }
 console.log(`The first element of array => ${arry()}`)
@@ -17,7 +17,7 @@ console.log(`The first element of array => ${arry()}`)
 // Create a function that takes an array and returns its last element.
 
 function arrthree(){
-    arr = [25,41,90,84,55]
+   let arr = [25,41,90,84,55]
     return arr.at(-1)
 }
 console.log(`The Last element of array =>${arrthree()}`)
@@ -26,7 +26,7 @@ console.log(`The Last element of array =>${arrthree()}`)
 // Create a function that takes an array and returns the number of elements in it.
 
 function numOfElements(){
-    arr = [5,6,3,7,2,1,67,32,78]
+   let arr = [5,6,3,7,2,1,67,32,78]
     return arr.length
 }
 console.log(`The number of elements is => ${numOfElements()}`)
@@ -34,7 +34,7 @@ console.log(`The number of elements is => ${numOfElements()}`)
 // Create a function that takes an array of numbers and returns the sum of the numbers.
 
 function arrsumofnum(){
-    arr = [5,1,2,3,4,6,7,8,9]
+   let arr = [5,1,2,3,4,6,7,8,9]
     let sum = 0 
     for (let i = 0; i<arr.length; i++){
         sum += arr[i]
@@ -45,7 +45,17 @@ console.log(`The Sum of elements is => ${arrsumofnum()}`)
 
 // Create a function that takes an array of numbers and returns the largest number.
 
-
+function lararrfind(){
+   let arr = [3,6,2,4,8,1,5]
+    let largest = arr[0]
+    for (let i = 1 ; i< arr.length; i++){
+        if(arr[i]>largest){
+            largest = arr[i]
+    }   
+  }
+  return largest
+}
+console.log(`The Largest  elements in array is  => ${lararrfind()}`)
 
 // Create a function that takes an array of numbers and returns the smallest number.
 
