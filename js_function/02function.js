6ecfba87617f898eb6a00cc2eb242e3c1ff203a1
Aditya Negi <01adityanegi@gmail.com>
2026-08-22 16:03:09 +0000

    02 Functions

--- js_function/02function.js
+++ js_function/02function.js
@@ -7,19 +7,29 @@ function valueArray(){
 console.log(valueArray())
 
 // Create a function that takes an array and returns its first element.
-function arrtwo(){
+
+function arry(){
     arr = [5,1,90,84,23]
     return arr[0]
 }
-console.log(`The first element of array ${arrtwo()}`)
+console.log(`The first element of array => ${arry()}`)
 
 // Create a function that takes an array and returns its last element.
 
+function arrthree(){
+    arr = [25,41,90,84,55]
+    return arr.at(-1)
+}
+console.log(`The Last element of array =>${arrthree()}`)
 
 
 // Create a function that takes an array and returns the number of elements in it.
 
-
+function numOfElements(){
+    arr = [5,6,3,7,2,1,67,32,78]
+    return arr.length
+}
+console.log(`The number of elements is => ${numOfElements()}`)
 
 // Create a function that takes an array of numbers and returns the sum of the numbers.
 
