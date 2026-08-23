e24d7b6ff2d15c0dce036fa4dfa1236d191583bf
Aditya Negi <01adityanegi@gmail.com>
2026-08-23 16:21:54 +0000

    Function with object

---
+++ js_function/03function.js
@@ -0,0 +1,46 @@
+// Create a function that takes a student object and prints the student's name.
+
+function stu(){
+    student = {
+        name : "Karan",
+        age : 21,
+        gender : "Male",
+        email : "karan@beserious.in"
+    }
+    return student.name
+}
+console.log(`The student name is ${stu()}`)
+
+// Create a function that takes a student object and prints the student's age.
+
+
+
+// Create a function that takes a student object and changes the student's age.
+
+
+
+// Create a function that takes a student object and changes the student's city.
+
+
+
+// Create a function that takes a student object and returns the student's marks.
+
+
+
+// Create a function that takes a product object and returns its price.
+
+
+
+// Create a function that takes a product object and increases its price by 100.
+
+
+
+// Create a function that takes a person object and returns their name and age.
+
+
+
+// Create a function that takes an object and returns the number of properties it has.
+
+
+
+// Create a function that takes an object and changes the value of one of its properties.
