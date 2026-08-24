bab24d228a1a0fcca2b58d4e2306378cd630cc50
Aditya Negi <01adityanegi@gmail.com>
2026-08-24 17:14:04 +0000

    Function with array and object

---
+++ js_function/04function.js
@@ -0,0 +1,45 @@
+// Create a function that takes the students array and returns the first student.
+
+function student(){
+    std =[
+        { name: "Rahul", age: 20, marks: 80 },
+        { name: "Aman", age: 21, marks: 65 },
+        { name: "Rohit", age: 19, marks: 90 }
+    ]
+    return std[0]
+}
+console.log(student())
+
+// Create a function that takes the students array and returns the last student.
+
+
+
+// Create a function that takes the students array and returns the name of the first student.
+
+
+
+// Create a function that takes the students array and returns the marks of the second student.
+
+
+
+// Create a function that takes the students array and returns the student whose marks are highest.
+
+
+
+// Create a function that takes the students array and returns the student whose marks are lowest.
+
+
+
+// Create a function that takes the students array and returns the names of students who scored more than 70.
+
+
+
+// Create a function that takes the students array and returns the average marks.
+
+
+
+// Create a function that takes the students array and changes the marks of a particular student.
+
+
+
+// Create a function that takes the students array and adds a new student.
