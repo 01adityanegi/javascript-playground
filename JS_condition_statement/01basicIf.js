69c437ae2deb41d11979b50fd0c7205ea8768863
Aditya Negi <01adityanegi@gmail.com>
2026-09-07 14:21:30 +0000

    If statement

---
+++ JS_condition_statement/01basicIf.js
@@ -0,0 +1,35 @@
+// Take a number and check whether it is positive.
+
+const num = 6
+if (num%2===0){
+    console.log("It is Positive")
+}
+
+
+// Take a number and check whether it is negative.
+
+
+
+// Take a number and check whether it is zero.
+
+
+
+// Take a person's age and check whether they are eligible to vote (18 or above).
+
+
+
+// Take a number and check whether it is even.
+
+
+
+// Take a number and check whether it is odd.
+
+
+
+// Take a student's marks and check whether they passed (passing marks = 33).
+
+
+
+// Take a temperature and check whether it is above 30°C.
+
+
